5c665f214f54b0df587399ca642d08125d578e7e
Faham Khan <kfaham@gmail.com>
2018-07-21 01:05:31 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var nodemailer = require('nodemailer');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'html')
+app.use(express.static(__dirname + '/views'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/public'));
+//Store all JS and CSS in Scripts folder.
+
+var cfenv = require('cfenv');
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //__dirname : It will resolve to your project folder.
+});
+
+app.post('/submitContact',function(req,res){
+  console.log("Got some info")
+  console.log(req.body.txtName)
+  var name=req.body.txtName
+  var email= req.body.txtEmail
+  var subject= req.body.txtSubject
+  var message= req.body.txtMessage
+  var transporter = nodemailer.createTransport({
+  service: 'gmail',
+    auth: {
+      user: 'mmkhann@gmail.com',
+      pass: 'llmmk098'
+    }
+  });
+
+  var mailOptions = {
+    from: 'mmkhann@gmail.com',
+    to: 'mmkhann@gmail.com',
+    subject: subject,
+    text: "name:"+name+", "+"email:"+email+", message: "+message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.redirect('/');
+});
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
---
+++ views/assets/javascript/contactus.js
@@ -0,0 +1,63 @@
+﻿/***
+* Contact Us page
+**/
+$(document).ready(function () {
+
+    $("#btnSubmit").click(function (e) {
+
+        //validate fields are not empty
+        var isValid = true;
+        $('#txtName,#txtEmail,#txtSubject,#txtMessage').each(function () {
+            if ($.trim($(this).val()) == '') {
+                isValid = false;
+                $(this).css({
+                    "border": "1px solid red",
+                    "background": "#FFCECE"
+                });
+            }
+            else {
+                $(this).css({
+                    "border": "",
+                    "background": ""
+                });
+            }
+        });
+        if (isValid == false) {
+            e.preventDefault();
+            return false;
+        }
+
+        var inputParam = {};
+        inputParam.p_Name = $('#txtName').val();
+        inputParam.p_Email = $('#txtEmail').val();
+        inputParam.p_Phone = "";
+        inputParam.p_Subject = $('#txtSubject').val();
+        inputParam.p_Message = $('#txtMessage').val();
+
+        $.ajax({
+            type: "POST",
+            url: "../WebServices/WebService.asmx/SendEmail",
+            data: JSON.stringify(inputParam),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                var JObject = $.parseJSON(response.d);
+
+                if (JObject.Error.length > 0) {
+                    alert(JObject.Error[0].ErrorMessage);
+
+                    // Clear fields
+                    $('#txtSubject').val("");
+                    $('#txtMessage').val("");
+                }
+
+            },
+            failure: function (response) {
+                alert(response.d);
+            }
+        });
+    });
+
+    
+
+});
